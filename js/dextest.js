13b5e70228d8ae464e3dbe0e4a0d75352b19ea8a
dexteraj <hfm.andy@gmail.com>
2014-10-31 08:22:54 -0600

    Create dextest.js

---
+++ js/dextest.js
@@ -0,0 +1,9 @@
+$(function() {
+    $("#box1_first").hover(
+    function() {
+        $(this).css('background-color', '#f0f0f0')
+    }, function() {
+        $(this).css('background-color', '#c0c0c0')
+    });
+});​
+
